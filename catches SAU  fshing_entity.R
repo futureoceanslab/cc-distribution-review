a426cda0f60f99c8e295a4f795c92fde61abb5c7
elenaojea <elenaojea@gmail.com>
2017-12-17 21:34:36 +0100

    I have been working on the codes and updating data from SAU. We didnt have all  the catches from the fishing_entities, only the catches in the EEZs of the review, so I am correcting that

--- catches SAU  fshing_entity.R
+++ catches SAU  fshing_entity.R
@@ -10,8 +10,8 @@ library(tidyr)
 data.1<-read.csv("data/Final SAU EEZ.csv", header=T, sep = ";")
 data.1<-tbl_df(data.1)
 
-#select data from 2010 to 2014
-data.2 <- filter(data.1, year > 2009)  ##NOTE THAT USA EAST COAST IS OUT SINCE THERE IS ONLY DATA UNTIL 1973 - this affects Depth catches
+#select data from 2010 to 2014 (because we average data for this period?)
+data.2 <- filter(data.1, year > 2009)  
 dat <- filter(data.2, catch_type=="Landings")
 dat[rowSums(is.na(dat)) != ncol(dat),]
 
@@ -21,12 +21,20 @@ SpReview<-as.character(unique(ReviewDat$b_scientific_name))
 
 ReviewDat$eez_countries[ReviewDat$eez_countries == "USA (Alaska, Arctic)"] <- "USA (Alaska-Subarctic)"
 ReviewDat$eez_countries[ReviewDat$eez_countries == "Spain (mainland, Med and Gulf of Cadiz)"] <- "Spain (mainland Med and Gulf of Cadiz)"
-ReviewDat$eez_countries[ReviewDat$eez_countries == "USA (West Coast)"] <- "USA (East Coast)"
+ReviewDat$eez_countries[ReviewDat$site == "Northeast US shelf"] <- "USA (East Coast)"
 ReviewDat$eez_countries[ReviewDat$eez_countries == "Denmark (North Sea), Germany (North Sea), Netherlands, Norway, United Kingdom (UK), Belgium, Sweden (West Coast)"] <- "Denmark (North Sea), Germany (North Sea), Netherlands, Norway, United Kingdom (UK), Belgium"
+ReviewDat$eez_countries[ReviewDat$eez_countries == "Canada (East Coast)\n"] <- "Canada (East Coast)"
+
+##some observations have no EZ. I put a EEZ based on location and http://www.marineregions.org/eezmapper.php
+##[6] Spain(Northwest), [166-175] France (Atlantic), [176,177] Norway; [178-189] US North East coast. 
+
+ReviewDat$eez_countries[6] <- "Spain (Northwest)" 
+ReviewDat$eez_countries[166:175] <- rep("France (Atlantic Coast)", 10)
+ReviewDat$eez_countries[176:177] <- rep("Norway", 2)
+ReviewDat$eez_countries[178:189] <- rep("USA (East Coast)", 12)
 
 
-
-#adapt names from datacountries$b_scientific_name to SAU
+#adapt names from datacountries$b_scientific_name to SAU (using WORMS)
 
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Seriola quinqueradiata"] <- "Seriola"
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Sebastes viviparus"] <- "Sebastes"
@@ -46,10 +54,52 @@ ReviewDat[162,11] <- "Lepidopsetta bilineata"
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Atherina presbyter"] <- "Atherina boyeri"
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Myoxocephalus octodecemspinosus"] <- "Cottidae"
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Loligo pealeii"] <- "Loligo"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Loligo opalescens"] <- "Doryteuthis opalescens"
+
+##these species do not appear in SAU so we use the more generic family
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Sebastes fasciatus"] <- "Sebastes"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Leucoraja ocellata"] <- "Raja"       #because they are <90cm
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Sebastes saxicola"] <- "Sebastes"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Stenotomus caprinus"] <- "Stenotomus chrysops"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Serranus atrobranchus"] <- "Serranus"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Penaeus duorarum"] <- "Penaeidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Penaeus aztecus"] <- "Penaeidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Amblyraja radiata"] <- "Raja"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Sebastes ruberrimus"] <- "Sebastes"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Sebastes brevispinis"] <- "Sebastes"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Raja rhina"] <- "Rajidae"  #greater than 90cm"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Raja binoculata"] <- "Rajidae"  #greater than 90cm"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Pandalus platyceros"] <- "Pandalus"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Isopsetta isolepis"] <- "Pleuronectidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Paralithodes platypus"] <- "Lithodidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Clupea pallasii"] <- "Clupea pallasii pallasii"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Pandalus eous"] <- "Pandalus"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Paralithodes aequispinus"] <- "Lithodidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Chionoecetes bairdi"] <- "Chionoecetes"
+
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Berryteuthis magister"] <- "Mollusca"  #general but present in the alaska/subartic
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Lepidopsetta\240polyxystra"] <- "Pleuronectidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Scophthalmidae"] <- "Scophthalmus aquosus"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Urophycis regia"] <- "Phycis"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Atheresthes\240stomias"] <- "Atheresthes stomias"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Buglossidium luteum"] <- "Soleidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Arnoglossus laterna"] <- "Bothidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Peprilus burti"] <- "Stromateidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Diplectrum bivittatum"] <- "Serranus"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Cyclopsetta chittendeni"] <- "Paralichthyidae"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Lithodes aequispinus"] <- "Lithodidae"
+
+
+
+#remove blank spaces from variable values in species
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+ReviewDat$b_scientific_name <- trim(ReviewDat$b_scientific_name)
+dat$scientific_name <- as.character(dat$scientific_name)
 
 #check the species list
 SpReview <- as.character(unique(ReviewDat$b_scientific_name))
 SpSeaAroundUs <- as.character(unique(dat$scientific_name))
+
 
 #como no hay match entre species names en ReviewDat y dat, elimino las que no estan
 matchsp <- SpReview %in% SpSeaAroundUs
@@ -57,15 +107,11 @@ table1 <- data.frame(matchsp, SpReview)
 table2 <- subset(table1, table1$matchsp==FALSE) ##species from review database missing in Sea Around Us
 
 
+
+
 #Create a new dataframe for the eez_countries info, keeping the id_obs
 EezCountries <- select(ReviewDat, id_obs, b_scientific_name, eez_countries)
 
-#EezMissing <- subset(EezCountries, EezCountries$b_scientific_name %in% table2$SpReview)
-
-#delete strange values in eez_country
-#EezCountries$eez_countries[EezCountries$eez_countries=="1"] <- NA
-#EezCountries$eez_countries[EezCountries$eez_countries=="10"] <- NA
-#EezCountries$eez_countries[EezCountries$eez_countries=="11"] <- NA
 
 #multiply the observations based on the number of eez_countries
 
