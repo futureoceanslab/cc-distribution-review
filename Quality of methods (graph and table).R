00f28942526ad1c20915f1e48d1fbf891e09d6ba
dsalgueirootero <Diegosalgueirootero@gmail.com>
2018-01-30 12:31:28 +0100

    Update Quality of methods (graph and table).R

--- Quality of methods (graph and table).R
+++ Quality of methods (graph and table).R
@@ -1,7 +1,7 @@
 
 ##Author: Diego Salgueiro Otero
-##Data: 15/01/2017
-##Title: The quality of methods for the review-paper
+##Data: 30/01/2017
+##Title: Script for methods quality for the review paper
 
 #Installing packages
 install.packages("gridExtra")
@@ -14,8 +14,9 @@ library(networkD3)
 library(gridExtra)
 library(data.table)
 library(grid)
+library(shiny)
 
-##Making a specific subset to plot the specific variables
+##Makeing a specific subset to plot the specific varibales
 mybiblio_database<-biblio_database[,c(2, 3,4, 6, 8,32,33,59, 63)]
 str(mybiblio_database)
 mybiblio_database<- as.data.frame(mybiblio_database)
@@ -27,8 +28,16 @@ mybiblio_database
 ##order in time 
 mybiblio_database<-mybiblio_database[order(mybiblio_database$article_year),]
 
-##creating new column
+##creating new column with the number of observations
 mybiblio_database$observations<-1 
+
+###### characteristics
+names(mybiblio_database)
+dim( mybiblio_database)
+head( mybiblio_database)
+tail ( mybiblio_database)
+str( mybiblio_database)
+summary( mybiblio_database)
 
 ##1st graph : obersvations-papers-duration of studies
 mybiblio_database$categoriesbyyears[mybiblio_database$b_years==10]<-"0-25"
@@ -63,46 +72,32 @@ mybiblio_database$data_type<- factor( mybiblio_database$data_type, levels=c("1",
                                       labels=c("Data collected","Biological surveys"))
 
 mybiblio_database$statistical_methodology<- factor( mybiblio_database$statistical_methodology, levels=c("1","2","5","23"),
-                                                    labels=c("Lineal correlation","Lineal regresion","Indirect relation", "Lineal regresion and Spatial analysis"))
+                                                    labels=c("Correlation","Regression","Indirect relation", "Regression and Spatial analysis"))
 
-##1st graph "observations-duration of studies-statistical methodology"
+
+##FIGURES
+
+##1st Figure: Stalked-Bar-Chart that shows "nº observations-duration of studies-statistical methodology"
 ggplot(data = mybiblio_database, aes(x = categoriesbyyears, y = observations, fill = statistical_methodology)) + 
   geom_bar(stat="identity", alpha=0.8) + scale_fill_manual(values=c("darkgreen", "yellow", "orange", "orangered")) +
-  labs(title="Quality of data-source", subtitle="496 obs.", y = "Number of observations", x = "Duration of the studies (Years)", fill = "Statistical methodology", caption="Review Future Oceans Lab")+ 
+  labs(title="Quality of data-source", y = "Number of observations", x = "Duration of the studies (Years)", fill = "Statistical methodology")+ 
   theme(panel.background = element_rect(fill = 'whitesmoke', colour = 'black'), legend.position= "left")
 
+##2d figure: Table with general information.
 ## Plotting a table including following variables: author, article_year, b_years, b_years_patern, data_type and statistical_methodology 
 #creating a new subset
-mytable<-mybiblio_database[,c(3,4,6,7,8,9)]
+mytable<-mybiblio_database[,c(3,4,6,7,8,9,10)]
 mytable
+##Re-order the columns in the dataframe
+mytable<-mytable[, c(1,2,7,3,4,5,6)]
 
-data<-mytable
-maxrow <-35
-npages <- ceiling(nrow(data)/maxrow)
-pdf("DT1.pdf", height = 11, width = 8.5)
-idx <- seq(1,maxrow)
-grid.table(data[idx,], rows = NULL)
-for(i in 2:npages){
-  grid.newpage()
-  if(i*maxrow <= nrow(data)){
-    idx <- seq(1+((i-1)*maxrow), i*maxrow)
-  }else{
-    idx <- seq(1+((i-1)*maxrow), nrow(data))
-  }
-  grid.table(data[idx,], rows = NULL)
-}
-dev.off()
+## Creating a database in excell (called "Methods_table") based on the information of mytable dataframe, but simplified.
+##Subsetting only the 26 first rows.
+Mytable<- Methods_table[c(1:26),]
+##Transforming the database in a dataframe
+Mytable<- as.data.frame(Mytable)
 
-
-##-----------------------
-
-###### characteristics
-names(mybiblio_database)
-dim( mybiblio_database)
-head( mybiblio_database)
-tail ( mybiblio_database)
-str( mybiblio_database)
-summary( mybiblio_database)
-
-
-
+##Exporting the second figure (table) as a pdf
+pdf("Description of database.pdf", height = 11, width = 13)
+grid.table(Mytable, rows=NULL)
+dev.off() 
