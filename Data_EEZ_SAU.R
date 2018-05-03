75b3153cb8858aad06a705a155be610cd2746feb
irrubio <iratxe.rubio@bc3research.org>
2018-05-03 10:20:31 +0200

    Merging EEZ csvs downloaded from SAU

--- Data_EEZ_SAU.R
+++ Data_EEZ_SAU.R
@@ -1,3 +1,8 @@
+#Creation of the EEZ Catches dataset: Final_SAU_EEZ.csv
+#source: EEZs that appear in the review database (included in Biblio_database.ppt)
+#source catch data: Sea Around Us
+#Iratxe Rubio, May 18
+
 library(tidyverse)
 path <- "data/data_EEZ_SAU/"
 l <- list.files(path, pattern=".csv")
@@ -9,6 +14,8 @@ func <- function(i){
 
 l1 <- lapply(1:length(l), func)
 final <- bind_rows(l1)
+
+#write.csv(final, file = "data/Final_SAU_EEZ.csv")
 
 #verification number of EEZ names and number of rows
 unique(final$area_name)
