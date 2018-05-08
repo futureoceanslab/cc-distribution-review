171145331b7c184e993301389678bd91cba2addc
dsalgueirootero <diegosalgueirootero@gmail.com>
2018-05-08 15:24:23 +0200

    Correction of problems

--- integration fishbase.R
+++ integration fishbase.R
@@ -36,13 +36,13 @@ ReviewDat$rfishbase_species_code[ReviewDat$rfishbase_species_code=="3"] <- NA
 
 ##Create a list of our species scientific names
 SpReviewOriginal <- unique(as.character(ReviewDat$b_scientific_name)) # Review_database. list of species from original database (146)
-SpCodes<-as.vector(unique(ReviewDat$rfishbase_species_code)) #Fishbase-spp codes. there are MISSING CODES (122 sp-codes included here) ( there are 2 NA)
+SpCodes<-as.vector(unique(ReviewDat$rfishbase_species_code)) #Fishbase-spp codes. there are MISSING CODES (122 sp-codes included here) ( there is 1 NA)
 SpReview <- unlist(species_names(SpCodes))  #Fishbase-spp names. only select the species where we have SpCodes (122 spp names matched)
 spdiff <- SpReviewOriginal %in% SpReview
 table(spdiff) # Synthesis of matches and mismatches between review-fishbase (122 true, 24 false)
 spmiss <- SpReviewOriginal[spdiff==FALSE] #species not included in the SpReview to take into account
 
-## To chek the matches among lists and edit the list of spp lost
+##To chek the matches among lists and edit the list of spp lost
 #write.csv(SpReviewOriginal, file="data/listSpReviewOriginal.csv")
 #write.csv(SpReview, file="data/listSpReview.csv")
 #write.csv(spmiss, file="data/1listspmiss.csv") ##list of lost species
@@ -80,6 +80,7 @@ colnames(ReviewDat)[23] <- "SpecCode"  #to put the same variable name in both da
 # Review+stockdat
 ReviewDatst <- left_join(ReviewDat, mystockdat, by = "StockCode")
 #Review+speciesdat
+ReviewDat$SpecCode<-as.integer(ReviewDat$SpecCode)
 ReviewDatsp <- left_join(ReviewDat, speciesDat, by = "SpecCode")
 
 #Save the FULL data (stockdat and speciesdat) of fishbase with our reviewdata
