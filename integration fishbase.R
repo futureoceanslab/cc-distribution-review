f1d90e55ec8c01ad1481eafb90f9afe6b598904b
dsalgueirootero <diegosalgueirootero@gmail.com>
2018-05-08 15:53:22 +0200

    Correction of 145 final species after apdate the scientific names

--- integration fishbase.R
+++ integration fishbase.R
@@ -31,21 +31,23 @@ ReviewDat$b_scientific_name <- trim.trailing(ReviewDat$b_scientific_name )
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Atheresthes\240stomias"] <- "Atheresthes stomias"
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Lepidopsetta\240polyxystra"] <- "Lepidopsetta polyxystra"
 ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Clupea pallasii"] <- "Clupea pallasii pallasii"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Loligo pealeii"] <- "Doryteuthis pealeii"
+ReviewDat$b_scientific_name[ReviewDat$b_scientific_name=="Loligo opalescens"] <- "Doryteuthis opalescens"
 ReviewDat$rfishbase_species_code[ReviewDat$rfishbase_species_code=="322"] <- "308"
 ReviewDat$rfishbase_species_code[ReviewDat$rfishbase_species_code=="3"] <- NA
 
 ##Create a list of our species scientific names
-SpReviewOriginal <- unique(as.character(ReviewDat$b_scientific_name)) # Review_database. list of species from original database (146)
+SpReviewOriginal <- unique(as.character(ReviewDat$b_scientific_name)) # Review_database. list of species from original database (145)
 SpCodes<-as.vector(unique(ReviewDat$rfishbase_species_code)) #Fishbase-spp codes. there are MISSING CODES (122 sp-codes included here) ( there is 1 NA)
 SpReview <- unlist(species_names(SpCodes))  #Fishbase-spp names. only select the species where we have SpCodes (122 spp names matched)
 spdiff <- SpReviewOriginal %in% SpReview
-table(spdiff) # Synthesis of matches and mismatches between review-fishbase (122 true, 24 false)
+table(spdiff) # Synthesis of matches and mismatches between review-fishbase (122 true, 23 false)
 spmiss <- SpReviewOriginal[spdiff==FALSE] #species not included in the SpReview to take into account
 
 ##To chek the matches among lists and edit the list of spp lost
 #write.csv(SpReviewOriginal, file="data/listSpReviewOriginal.csv")
 #write.csv(SpReview, file="data/listSpReview.csv")
-#write.csv(spmiss, file="data/1listspmiss.csv") ##list of lost species
+write.csv(spmiss, file="data/1listspmiss.csv") ##list of lost species
 
 ##GET INFO FROM FISHBASE
 ##getting SPECIES LEVEL data from Fishbase to our species list (122 spp out of 146 spp)
