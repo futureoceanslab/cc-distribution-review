d0704342ed3e2fc0bcc6ff7d81ae70721d85770b
irrubio <iratxe.rubio@bc3research.org>
2019-02-28 17:03:52 +0100

    Changes

--- 3.1_Distribution_impacts.R
+++ 3.1_Distribution_impacts.R
@@ -12,76 +12,17 @@ library(reshape2) #melt function
 
 #open dataset
 table<-read.table("data/biblio_database.csv", header= T, sep= ",")
-##Subset of biblio_database without duplicated data
-##table1<- table1%>%
-##  filter(table1$duplicate=="1")
-str(table)
 
 #Factors
-table$researcher<-as.factor(table$researcher)
-table$id_study<-as.factor(table$id_study)
-table$cc<-as.factor(table$cc)
-table$b_impact<-as.factor(table$b_impact)
-table$b_direction<-as.factor(table$b_direction)
+#table$researcher<-as.factor(table$researcher)
+#table$id_study<-as.factor(table$id_study)
+#table$cc<-as.factor(table$cc)
+#table$b_impact<-as.factor(table$b_impact)
+#table$b_direction<-as.factor(table$b_direction)
 
 #Numeric
-table$b_value<-as.numeric(as.character(table$b_value), na.omit=TRUE)
-table$b_years<-as.numeric(as.character(table$b_years))
-
-#change codes to a short description; in our review center of grav separated from center of biomass
-levels(table$b_impact)<-c("lat shift center of grav", #1
-                          "lat shift center of bio",  #2
-                          "depth shift", #3
-                          "boundary lat shift center of grav", #4
-                          "boundary lat shift center of bio", #5
-                          "long shift center of grav", #7
-                          "long shift center of bio", #8
-                          "lat and long shift center of grav", #9
-                          "shift in area occupied") #11
-
-levels(table$b_direction)<-c("lat shift north center of grav", #1
-                             "lat shift south center of grav", #2
-                             "boundary lat shift north center of grav", #3
-                             "max lat shift north center of grav", #4
-                             "max lat shift south center of grav",#5
-                             "min lat shift north center of grav", #6
-                             "shifting deeper", #7
-                             "shifting shallower", #8
-                             "area expansion", #9
-                             "area contraction", #10
-                             "lat shift north center of bio", #11
-                             "lat shift south center of bio", #12
-                             "shift southweastwards center of grav", #13
-                             "shift northeastwards center of grav", #14
-                             "shift westwards center of grav", #19
-                             "boundary lat shift east center of bio", #20
-                             "boundary lat shift north center of bio") #21
-
-levels(table$cc)<-c("AMO", #10
-                    "climate velocity", #11
-                    "sst", #2
-                    "sst,bt,AMO", #2,3,10
-                    "sst,bt", #2,3
-                    "bt") #3
-
-#new colums with center of grav combined with center of biomass
-table["b_impact_combine"]<-table$b_impact
-table$b_impact_combine<-combineLevels(table$b_impact_combine, levs = c("lat shift center of grav", "lat shift center of bio"), newLabel = c("lat shift"))
-table$b_impact_combine<-combineLevels(table$b_impact_combine, levs = c("boundary lat shift center of grav", "boundary lat shift center of bio"), newLabel = c("boundary lat shift"))
-table$b_impact_combine<-combineLevels(table$b_impact_combine, levs = c("long shift center of grav", "long shift center of bio"), newLabel = c("long shift"))
-levels(table$b_impact_combine)[levels(table$b_impact_combine)=="lat and long shift center of grav"] <- "lat and long shift"
-
-table["b_direction_combine"]<-table$b_direction
-table$b_direction_combine<-combineLevels(table$b_direction_combine, levs = c("lat shift north center of grav", "lat shift north center of bio"), newLabel = c("lat shift north"))
-table$b_direction_combine<-combineLevels(table$b_direction_combine, levs = c("lat shift south center of grav", "lat shift south center of bio"), newLabel = c("lat shift south"))
-table$b_direction_combine<-combineLevels(table$b_direction_combine, levs = c("boundary lat shift north center of grav", "boundary lat shift north center of bio"), newLabel= c("boundary lat shift north"))
-levels(table$b_direction_combine)[levels(table$b_direction_combine)=="max lat shift north center of grav"] <- "max lat shift north"
-levels(table$b_direction_combine)[levels(table$b_direction_combine)=="max lat shift south center of grav"] <- "max lat shift south"
-levels(table$b_direction_combine)[levels(table$b_direction_combine)=="min lat shift north center of grav"] <- "min lat shift north"
-levels(table$b_direction_combine)[levels(table$b_direction_combine)=="shift southweastwards center of grav"] <- "shift southweastwards"
-levels(table$b_direction_combine)[levels(table$b_direction_combine)=="shift northeastwards center of grav"] <- "shift northeastwards"
-levels(table$b_direction_combine)[levels(table$b_direction_combine)=="shift westwards center of grav"] <- "shift westwards"
-levels(table$b_direction_combine)[levels(table$b_direction_combine)=="boundary lat shift east center of bio"] <- "boundary lat shift east"
+#table$b_value<-as.numeric(as.character(table$b_value), na.omit=TRUE)
+#table$b_years<-as.numeric(as.character(table$b_years))
 
 #SUBSETS COMBINE
 lat<-subset (table, b_impact_combine=="lat shift")
@@ -113,12 +54,7 @@ ggplot(data = data1, aes(x = impacts, y = value, fill = factor(cc))) +
 
 
 #same figure without climate velocity
-data2 <- subset (data1,  ! cc == "Climate_Velocity") # same dataset without climate velocity
-
-data2$cc <- factor(data2$cc, levels = c("sst_bt","AMO","sst", "sst_bt_AMO",
-                                        "bt"), labels=c("SST and BT", "AMO", "SST", "SST, BT and AMO", "BT"))
-data2$impacts <- factor (data2$impacts, levels= c ("Latitude", 
-                                                 "Depth","Boundary", "Area"), labels=c("Mean Latitude", "Depth", "Boundary Latitude", "Area"))                                       
+data2 <- subset (data1,  ! cc == "climate velocity") # same dataset without climate velocity
 
 ggplot(data = data2, aes(x = impacts, y = value, fill = factor(cc))) + 
   geom_bar(stat="identity", alpha=0.8) + scale_fill_manual(values=c("darkgreen", "chartreuse3", "yellow", "orange", "orangered", "red3")) +
