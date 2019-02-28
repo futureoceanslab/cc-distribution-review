dee8615c0fee9ecc6717b36c9a86f23c38d88569
irrubio <iratxe.rubio@bc3research.org>
2019-02-28 12:21:36 +0100

    adding code

--- 1_Duplications.R
+++ 1_Duplications.R
@@ -4,3 +4,7 @@ table <- read.table("data/biblio_database.csv", header = T, sep = ",")
 
 table %>%
   distinct(var1, var2, .keep_all = TRUE)
+
+##Subset of biblio_database without duplicated data
+##table1<- table1%>%
+##  filter(table1$duplicate=="1")
