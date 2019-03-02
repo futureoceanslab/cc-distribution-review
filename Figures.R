753ec8c7d89f61714bd702db9ac8a2ab1f715ba9
dsalgueirootero <diegosalgueirootero@gmail.com>
2019-03-02 10:49:55 +0100

    Modifications on Figures

--- Figures.R
+++ Figures.R
@@ -1,5 +1,5 @@
 ##R code for figures
-##Author: FOL DS
+##Author: FOL
 ##Date: March, 1st 2019
 
 ##calling libraries
@@ -14,47 +14,8 @@ library(data.table)
 library(grid)
 library(ggrepel)
 
-########################################### graphs_rest_SAU
-
-########################################### Graphs lat SAU
-###########################################
-##Catch dependency
-#December 2017
-#source: Biblio_database_full.csv
-#output: figures impacts catch dependency, catch pressure
-
 #Open Biblio_data with SAU data on EEZ and FE:
 data <- read.csv("data/Biblio_database_full.csv", stringsAsFactors = FALSE)
-
-##CREATION OF VARIABLES for DEPEDENCE
-# A) CATCH DEPENDENCY OF FISHING ENTITIES
-# B) VALUE OF SPECIES FOR FISHING ENTITIES
-# C) CATCH PRESSURE IN EEZ
-
-# A) CATCH DEPENDENCY OF FISHING ENTITIES
-#1. Species catch dependency on the area
-data$catchdepFEsp <- data$tonnesFEsp/data$tonnesFEspT #the dependence of the country species catches on the EEZ species catches
-data$landdepFEsp <-  data$landedvalueFEsp/data$landedvalueFEspT #the dependence of the country total SP Value on the EEZ SP catch value 
-range(data$catchdepFEsp, na.rm=TRUE)
-range(data$landdepFEsp, na.rm=TRUE) #is the same relation
-#2. Country dependency on the species in the area
-data$catchdepFE <- data$tonnesFEsp/data$tonnesFE #the dependence of the country species catches on the EEZ species catches
-range(data$catchdepFE, na.rm=TRUE)
-#3. Country dependency on the area
-data$catchdepFEEZ <- data$tonnesFEEZ/data$tonnesEEZ 
-range(data$catchdepFEEZ, na.rm=TRUE)
-a<-filter(data,catchdepFEEZ>1)
-
-# B) VALUE OF SPECIES FOR FISHING ENTITIES
-###Species VAlue in FE: landed value/tonnes
-data$spvalueFE  <- data$landedvalueFEsp/data$tonnesFEsp
-range(data$spvalueFE, na.rm=TRUE)
-quantile(data$spvalueFE, na.rm=TRUE)
-
-# C) CATCH PRESSURE IN EEZ
-data$catchpresEEZsp <-data$tonnesEEZsp/data$tonnesEEZ
-data$landpresEEZsp <- data$landedvalueEEZsp/data$landedvalueEEZ
-range(data$catchpresEEZsp, na.rm=TRUE)
 
 ##PREPARETION OF VARIABLES: Subsets of the impacts
 
@@ -73,7 +34,7 @@ range    <- subset (data, b_impact== "boundary lat shift")
 area     <- subset (data, b_impact == "area occupied")
 
 
-########### FIGURES FOR CATCH and PRICE CATEGORY IMPACTS ###########
+########### FIGURES 3 and 4: CATCH and PRICE CATEGORY IMPACTS ###########
 ####LATITUDE
 latitude$area_name <- as.factor(latitude$area_name)
 
@@ -266,73 +227,47 @@ grid.arrange(P1, P2, P5, P6, nrow = 2, ncol=2)
 grid.arrange(P3,P4,P7,P8, nrow = 2, ncol=2)
 
 
-#########RELATIONAL DEPENDENCY FIGURE : FISHING ENTITIES AND EEZ (T AND $) #####################
+
+######### FIGURE 5: IMPACTS AND FE DEPENDENCY ON SHIFTING STOCKS #################
+
+
+
+##PAPER Fig.5
+grid.arrange(P9, P10, nrow = 1, ncol=2)
+
+
+
+######### FIGURE 6: RELATIONAL DEPENDENCY between FISHING ENTITIES AND EEZ (T AND $) #####################
+
+##CREATION OF VARIABLES for DEPEDENCE
+data$catchdepFEEZ <- data$tonnesFEEZ/data$tonnesEEZ 
+range(data$catchdepFEEZ, na.rm=TRUE)
+
 ##plot catch dependency
-ggplot(data, aes(x = area_name, y = fishing_entity, fill = catchdepFEEZ)) +
+P11<- ggplot(data, aes(x = area_name, y = fishing_entity, fill = catchdepFEEZ)) +
   geom_tile(data = subset(data, !is.na(fishing_entity))) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
   scale_fill_gradientn("Dependency (t)", colours = rev(brewer.pal(9, "Spectral"))) +
   xlab("Economic Exclusive Zones") + ylab("Fishing entities")
+P11
 
 ##plot $ dependency
-ggplot(data, aes(x = area_name, y = fishing_entity, fill = landedvalueFEEZ/landedvalueFE)) +
+P12<- ggplot(data, aes(x = area_name, y = fishing_entity, fill = landedvalueFEEZ/landedvalueFE)) +
   geom_tile(data = subset(data, !is.na(fishing_entity))) +
   theme_bw() +
   theme( axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
   scale_fill_gradientn("Dependency ($)", colours = rev(brewer.pal(9, "Spectral"))) +
   xlab("Economic Exclusive Zones") + ylab("Fishing entities")
-
-#1 plot relation Countries-fishing grounds by catches (tonnes)
-relation1 <- ggplot(Biblio_database_full, aes(x = area_name, y = fishing_entity, fill = tonnesFEsp)) +
-  geom_tile(data = subset(Biblio_database_full, !is.na(fishing_entity))) +
-  theme_bw() +
-  theme( axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-  scale_fill_gradientn("Catches (tonnes)", colours = rev(brewer.pal(9, "RdYlBu"))) +
-  xlab("Fishing grounds") + ylab("Countries")
-relation1
-
-#2 plot relation Countries-fishing grounds by landed value (USD)
-relation2 <- ggplot(Biblio_database_full, aes(x = area_name, y = fishing_entity, fill = landedvalueFEsp)) +
-  geom_tile(data = subset(Biblio_database_full, !is.na(fishing_entity))) +
-  theme_bw() +
-  theme(  axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-  scale_fill_gradientn("Landed value (USD)", colours = rev(brewer.pal(9, "Spectral")), limits=c(0,420000000)) +
-  xlab("Fishing grounds") + ylab("Countries")
-relation2
-
-##create variable of dependence
-Biblio_database_full$catchdepFEsp<- Biblio_database_full$tonnesFEsp/Biblio_database_full$tonnesFEspT
-Biblio_database_full$landdepFEsp<- Biblio_database_full$landedvalueFEsp/Biblio_database_full$tonnesFEspT
-range(Biblio_database_full$catchdepFEsp, na.rm=TRUE)
-range(Biblio_database_full$landdepFEsp, na.rm=TRUE) 
-
-#3 plot ecnomic dependency
-relation3 <- ggplot(Biblio_database_full, aes(x = area_name, y = fishing_entity, fill = catchdepFEsp)) +
-  geom_tile(data = subset(Biblio_database_full, !is.na(fishing_entity))) +
-  theme_bw() +
-  theme(  axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-  scale_fill_gradientn("Dependency (tonnes)", colours = rev(brewer.pal(9, "Spectral"))) +
-  xlab("Fishing grounds") + ylab("Countries")
-relation3
-
-#4 plot catches dependency
-relation4 <- ggplot(Biblio_database_full, aes(x = area_name, y = fishing_entity, fill = landdepFEsp)) +
-  geom_tile(data = subset(Biblio_database_full, !is.na(fishing_entity))) +
-  theme_bw() +
-  theme( axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-  scale_fill_gradientn("Dependency (USD)", colours = rev(brewer.pal(9, "Spectral"))) +
-  xlab("Fishing grounds") + ylab("Countries")
-relation4
+P12
 ## If we want to modify scale, code here: ", limits=c(0,1250000), breaks=seq(0,1250000, by=1000000)"
 
-##PAPER Fig.5 !!!!
-grid.arrange(P3,P4, nrow = 1, ncol=2)
-##PAPER Fig.6!!!!
-grid.arrange(P1, P2, nrow = 1, ncol=2)
+##PAPER Fig.6 
+grid.arrange(P11,P12, nrow = 1, ncol=2)
 
 
-##################################### SUPPLEMENTARY MATERIALS DS #############################################
+
+##################################### SUPPLEMENTARY MATERIALS #############################################
 
 ##Making a specific subset to plot the specific varibales
 mybiblio_database <- read.csv("data/biblio_database.csv", stringsAsFactors=FALSE, header=T, sep = ",")
@@ -352,11 +287,7 @@ mybiblio_database$observations<-1
 
 ###### characteristics
 names(mybiblio_database)
-dim( mybiblio_database)
-head( mybiblio_database)
-tail ( mybiblio_database)
-str( mybiblio_database)
-summary( mybiblio_database)
+glimpse( mybiblio_database)
 
 ##1st graph : obersvations-papers-duration of studies
 mybiblio_database$categoriesbyyears[mybiblio_database$b_years==10]<-"0-25"
